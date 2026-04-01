15be88f82a37c1293a30254a4f2b6d005ca52cda
kielynwilson <wilson.kielyn@outlook.com>
2026-03-31 22:02:08 -0700

    Create App class with main and add methods

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Pipeline app is running.");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
